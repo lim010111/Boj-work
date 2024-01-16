18f2ab949e505b0e8649949bd6c6b47347cc7b1e
lim010111 <87302623+lim010111@users.noreply.github.com>
2024-01-16 23:08:53 +0900

    [Silver I] Title: 회의실 배정, Time: 4104 ms, Memory: 51900 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,16 +1,16 @@
 n = int(input())
-li = []
+meetings = []
 for i in range(n):
-    li.append(tuple(map(int, input().split())))
+    meetings.append(tuple(map(int, input().split())))
 
-li.sort(key=lambda x:(x[1], x[0]))
+meetings.sort(key=lambda x:(x[1], x[0]))
 
-max_cnt = 1
-selected = li[0]
+cnt = 1
+selected = meetings[0] # 맨 앞의 회의를 변수에 저장
 
 for i in range(1, n):
-    if li[i][0] >= selected[1]:
-        max_cnt += 1
-        selected = li[i]
+    if meetings[i][0] >= selected[1]: # 방금 끝난 회의의 시간보다 시작 시간이 크거나 같으면 개수에 추가
+        cnt += 1
+        selected = meetings[i]
 
-print(max_cnt)
+print(cnt)
