18caa7dac41769b393a980fe446fdeef3d2906f8
lim010111 <87302623+lim010111@users.noreply.github.com>
2025-02-06 15:58:40 +0900

    [Silver I] Title: 아침 태권도, Time: 92 ms, Memory: 109544 KB, Score: 10 point -BaekjoonHub

--- 백준/Silver/29197. 아침 태권도/아침 태권도.py
+++ 백준/Silver/29197. 아침 태권도/아침 태권도.py
@@ -4,14 +4,14 @@ from sys import stdin
 input = stdin.readline
 n = int(input())
 
-slope_set = set()
+slope_dict = {}
 
 for _ in range(n):
     x, y = map(int, input().split())
-    if y == 0:
-        slope = "infinite"
-    else:
-        slope = y / x
-    slope_set.add(slope)
 
-print(len(slope_set))
+    if y == 0:
+        slope_dict["infinite"] = True
+        continue
+    slope_dict[x / y] = True
+
+print(len(slope_dict))
