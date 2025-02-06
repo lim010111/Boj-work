358aeaae1049ffd7bf25e3810b788442ce2fa403
lim010111 <87302623+lim010111@users.noreply.github.com>
2025-02-06 16:12:49 +0900

    [Silver I] Title: 아침 태권도, Time: 336 ms, Memory: 60064 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/29197. 아침 태권도/아침 태권도.py
+++ 백준/Silver/29197. 아침 태권도/아침 태권도.py
@@ -4,14 +4,34 @@ from sys import stdin
 input = stdin.readline
 n = int(input())
 
-slope_set = set()
+vector_set = set()
 
 for _ in range(n):
     x, y = map(int, input().split())
-    if y == 0:
-        slope = "infinite"
-    else:
-        slope = y / x
-    slope_set.add(slope)
 
-print(len(slope_set))
+    if y == 0 and x > 0:
+        vector = (0, "right")
+    elif y == 0 and x < 0:
+        vector = (0, "left")
+    elif x == 0 and y > 0:
+        vector = (0, "top")
+    elif x == 0 and y < 0:
+        vector = (0, "bottom")
+    else:
+        if x > 0:
+            if y > 0:
+                q = 1
+            else:
+                q = 4
+        else:
+            if y > 0:
+                q = 2
+            else:
+                q = 3
+                
+        vector = (q, y / x)
+
+    vector_set.add(vector)
+    
+
+print(len(vector_set))
