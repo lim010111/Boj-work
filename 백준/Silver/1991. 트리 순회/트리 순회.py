3d8aa7de91a242ede4d58266506510226552f33a
lim010111 <87302623+lim010111@users.noreply.github.com>
2025-02-09 13:50:09 +0900

    [Silver I] Title: 트리 순회, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, data, left_node, right_node):
+        self.data = data
+        self.left_node = left_node
+        self.right_node = right_node
+
+
+def pre_order(node: Node) -> None:
+    print(node.data, end="")
+    if node.left_node != None:
+        pre_order(tree[node.left_node])
+    if node.right_node != None:
+        pre_order(tree[node.right_node])
+
+
+def in_order(node: Node) -> None:
+    if node.left_node != None:
+        in_order(tree[node.left_node])
+    print(node.data, end="")
+    if node.right_node != None:
+        in_order(tree[node.right_node])
+
+
+def post_order(node: Node) -> None:
+    if node.left_node != None:
+        post_order(tree[node.left_node])
+    if node.right_node != None:
+        post_order(tree[node.right_node])
+    print(node.data, end="")
+
+
+n = int(input())
+tree = {}
+
+for _ in range(n):
+    node, left_node, right_node = input().split()
+    if left_node == ".":
+        left_node = None
+    if right_node == ".":
+        right_node = None
+    tree[node] = Node(node, left_node, right_node)
+
+pre_order(tree["A"])
+print()
+in_order(tree["A"])
+print()
+post_order(tree["A"])
