3f5d83847b9702ac75aa42e3fe115b63ff865f5e
lim010111 <87302623+lim010111@users.noreply.github.com>
2025-06-20 13:32:15 +0900

    [Silver III] Title: 1로 만들기, Time: 112 ms, Memory: 118576 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,21 +1,19 @@
-from collections import deque
-
 def solve(n):
-    queue = deque([(n, 0)])
+    # 1 ~ n까지의 n개의 dp 테이블, 0은 무시
+    dp = [0] * (n + 1)
 
-    while queue:
-        cur, count = queue.popleft()
-        if cur == 1:
-            return count
 
-        if cur % 3 == 0:
-            queue.append((cur // 3, count + 1))
+    dp[1] = 0
+    # 2에서 시작해서 n까지, 순차적으로 최소 연산 횟수 구하기.
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + 1
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i // 2] + 1)
 
-        if cur % 2 == 0:
-            queue.append((cur // 2, count + 1))
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i // 3] + 1)
 
-        queue.append((cur - 1, count + 1))
-        
+    return dp[n]
     
 
 
