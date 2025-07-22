4e4b30d476cc7d6cd0f060d417cd3cc32acf5a43
lim010111 <87302623+lim010111@users.noreply.github.com>
2025-07-22 14:23:28 +0900

    [Silver I] Title: 미로 탐색, Time: 124 ms, Memory: 114288 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,40 +1,42 @@
-import sys
 from collections import deque
 
-input = sys.stdin.readline
-
-
-def bfs(x, y):
-    queue = deque()
-    queue.append((x, y))
+def bfs(n, m):
+    matrix = [list(map(int, list(input()))) for _ in range(n)]
+    moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    
+    start = None
+    for r in range(n):
+        for c in range(m):
+            if matrix[r][c] == 1:
+                start = (r, c)
+                break
+        if start is not None:
+            break
+    
+    
+    queue: deque[tuple] = deque([start])
 
     while queue:
-        x, y = queue.popleft()
+        r, c = queue.popleft()
 
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+        for move in moves:
+            nr = r + move[0]
+            nc = c + move[1]
 
-            if nx < 0 or ny < 0 or nx > n-1 or ny > m-1:
+            if nr < 0 or nc < 0 or nr >= n or nc >= m:
                 continue
 
-            if graph[nx][ny] == 0:
+            if matrix[nr][nc] != 1:
                 continue
 
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                
-                queue.append((nx, ny))
+            matrix[nr][nc] += matrix[r][c]
+            if (nr, nc) == (n - 1, m - 1):
+                return matrix[nr][nc]
+            
+            queue.append((nr, nc))
 
-    return graph[n - 1][m - 1]
 
+if __name__ == "__main__":
+    n, m = map(int, input().split())
 
-n, m = map(int, input().split())
-graph = []
-for _ in range(n):
-    graph.append(list(map(int, input().rstrip())))
-
-dx = [-1, +1, 0, 0]
-dy = [0, 0, -1, +1]
-
-print(bfs(0, 0))
+    print(bfs(n, m))
