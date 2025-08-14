08bc73c2851b52fd79e2961c84218521131e584d
lim010111 <87302623+lim010111@users.noreply.github.com>
2025-08-14 16:44:19 +0900

    [Silver V] Title: 올림픽, Time: 124 ms, Memory: 38476 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -3,12 +3,15 @@ from typing import List, Tuple
 def print_rank(info_list: List[Tuple[int]], k: int) -> int:
     info_list = sorted(info_list, key=lambda info:(-info[1], -info[2], -info[3]))
 
+
+    if info_list[0][0] == k:
+        return 1
+        
     rank = 1
     for i in range(1, n):
         if info_list[i - 1][1:] != info_list[i][1:]:
             rank = i + 1
-
-        if i + 1 == k:
+        if info_list[i][0] == k:
             return rank
         
 
