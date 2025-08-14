20868c8b8bdcf6dcb3cc718f231672d641fe2fe2
lim010111 <87302623+lim010111@users.noreply.github.com>
2025-08-14 16:34:52 +0900

    [Silver V] Title: 올림픽, Time: 180 ms, Memory: 119160 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -0,0 +1,19 @@
+from typing import List, Tuple
+
+def print_rank(info_list: List[Tuple[int]], k: int) -> int:
+    info_list = sorted(info_list, key=lambda info:(-info[1], -info[2], -info[3]))
+
+    rank = 1
+    for i in range(1, n):
+        if info_list[i - 1][1:] != info_list[i][1:]:
+            rank = i + 1
+
+        if i + 1 == k:
+            return rank
+        
+
+if __name__ == "__main__":
+    n, k = map(int, input().split())
+    info_list = [tuple(map(int, input().split())) for _ in range(n)]
+    result = print_rank(info_list, k)
+    print(result)
