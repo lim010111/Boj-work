07edbc344e6d81a8733665669d1680b0b319cf49
임우현_T8168 <87302623+lim010111@users.noreply.github.com>
2025-09-05 14:16:00 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.89 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,26 +1,41 @@
+from typing import List
 from collections import deque
 
-def solution(maps):
-    rows, cols = (len(maps), len(maps[0]))
-    moves = [(1, 0), (-1, 0), (0, -1), (0, 1)]
-    queue = deque([(0, 0)])
-    maps[0][0] = 1
-
-    while queue:
-        r, c = queue.popleft()
-        if (r, c) == (rows - 1, cols - 1):
-            return maps[r][c]
-        for move in moves:
-            next_r = r + move[0]
-            next_c = c + move[1]
-
-            if next_r < 0 or next_r >= rows or next_c < 0 or next_c >= cols:
-                continue
-        
-            if maps[next_r][next_c] != 1:
-                continue
-
-            maps[next_r][next_c] = maps[r][c] + 1
-            queue.append((next_r, next_c))
+def solution(maps: List[List[int]]) -> int:
+    answer = 0
     
-    return -1
+    n, m = len(maps), len(maps[0])
+    
+    visited = [[False] * m for _ in range(n)]
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+    
+    queue = deque([(0, 0)])
+    visited[0][0] = True
+    
+    while queue:
+        x, y = queue.popleft()
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if nx < 0 or nx > n - 1 or ny < 0 or ny > m - 1:
+                continue
+            
+            if visited[nx][ny]:
+                continue
+                
+            if maps[nx][ny] == 0:
+                continue
+                
+            queue.append((nx, ny))
+            maps[nx][ny] = maps[x][y] + 1
+            visited[nx][ny] = True
+            
+        if maps[-1][-1] == 1:
+            answer = -1
+        else:
+            answer = maps[-1][-1]
+    
+    return answer
