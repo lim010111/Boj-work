380ef521c28af3da42e86de80cd3744c808a8f41
임우현_T8168 <87302623+lim010111@users.noreply.github.com>
2025-09-12 16:45:40 +0900

    [level 3] Title: 불량 사용자, Time: 193.60 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -1,9 +1,29 @@
+'''
+["frodo", "fradi", "crodo", "abc123", "frodoc"] -> ["fr*d*", "abc1**"]
+
+[0, 0]
+[frodo, 0] -> [frodo, abc123] 끝
+[fradi, 0] -> [fradi abc123] 끝
+
+중복 제거 위해 sorted 처리 후, 조합에 추가
+'''
+
 def solution(user_id, banned_id):
     combs = set()
 
-    # 백트레킹:
-    # []랑 현재 index 넣고, 현재 인덱스에 맞는 banned_id랑 비교하며 재귀로 [] 채우기
-    def fill_combs(current_comb, depth, ban_idx):
+    def is_banned_user(id, target_id):
+        if len(id) != len(target_id):
+            return False
+        
+        for i in range(len(id)):
+                if target_id[i] == '*':
+                    continue
+                if id[i] != target_id[i]:
+                    return False
+        
+        return True
+
+    def fill_combs(current_comb, ban_idx):
         if ban_idx == len(banned_id):
             combs.add(tuple(sorted(current_comb[:])))
             return
@@ -11,23 +31,12 @@ def solution(user_id, banned_id):
         for i in range(len(user_id)):
             if user_id[i] in current_comb:
                 continue
-            # TODO: banned_id[ban_idx]와 user_id[i]의 fit 판단
+
             if is_banned_user(user_id[i], banned_id[ban_idx]):
                 current_comb.append(user_id[i])
-                fill_combs(current_comb, depth + 1, ban_idx + 1)
+                fill_combs(current_comb, ban_idx + 1)
                 current_comb.pop()
     
-    def is_banned_user(id, covered_id):
-        if len(id) != len(covered_id):
-            return False
-        
-        for i in range(len(id)):
-                if covered_id[i] == '*':
-                    continue
-                if id[i] != covered_id[i]:
-                    return False
-        
-        return True
                 
-    fill_combs([], 0, 0)
+    fill_combs([], 0)
     return len(combs)
